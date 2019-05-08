c191e86aba8dc284dc1e9b55bab485371b616d61
Illya Velichko <bitbetmy@gmail.com>
2019-05-08 10:14:02 +0300

    Initial commit

---
+++ src/Square/Square.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './index.css';
+export const Square = ( props ) => {
+    return (
+        <button className='square' onClick={props.onClick}>
+            {props.value}
+        </button>
+    )
+};
---
+++ src/Title/Title.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const styles = {
+  display: 'flex',
+  justifyContent: 'center',
+  color: 'red',
+};
+
+export const Title = ( props ) => <h3 style={{...styles}}>Tic-Tae toe</h3>;
